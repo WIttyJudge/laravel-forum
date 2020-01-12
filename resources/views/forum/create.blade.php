fcefa0b9383c0d660d6ad6d9cace5c72f83de1c7
vagrant <vagrant@homestead>
2020-01-12 17:36:33 +0000

    Create tag section in the page of creating the new thread.

--- resources/views/forum/create.blade.php
+++ resources/views/forum/create.blade.php
@@ -3,60 +3,57 @@
 @section('title', 'Create Thread')
 
 @section('content')
-    @include('partials.search-field')
+@include('partials.search-field')
 
-    <div class="h-screen bg-gray-200 pt-8">
-	    <div class="container mx-auto">
-            <div class="flex justify-center">
-                <div class="md:w-1/3 lg:w-1/2 my-8 md:my-20">
+<div class="h-screen bg-gray-200 pt-8">
+    <div class="container mx-auto">
+        <div class="flex justify-center">
+            <div class="md:w-1/3 lg:w-1/2 my-8 md:my-20">
 
-                    <div class="flex flex-col p-4 shadow-xl bg-white rounded">
-                        <form action="{{ route('forum.store') }}" method="POST">
-                            @csrf
+                <div class="flex flex-col p-4 shadow-xl bg-white rounded">
+                    <form action="{{ route('forum.store') }}" method="POST">
+                        @csrf
 
-                            <div class="mb-4">
-                                <label for="title" class="mb-1 text-sm text-gray-700 uppercase">Title</label>
-                                <input type="text" name="title" id="title" class="w-full p-3 rounded border-2" maxlength="60" required>
-                                <span class="text-gray-600 text-sm">Maximum 60 characters.</span>
+                        <div class="mb-4">
+                            <label for="title" class="mb-1 text-sm text-gray-700 uppercase">Title</label>
+                            <input type="text" name="title" id="title" class="w-full p-3 rounded border-2"
+                                maxlength="60" required>
+                            <span class="text-gray-600 text-sm">Maximum 60 characters.</span>
 
-                                @error('title')
-                                    <div class="text-red-500">{{ $message }}</div>
-                                @enderror
-                            </div>
+                            @error('title')
+                            <div class="text-red-500">{{ $message }}</div>
+                            @enderror
+                        </div>
 
 
-                            <div class="flex flex-col mb-4">
-                                <label for="body" class="mb-1 text-sm text-gray-700 uppercase">Body</label>
-                                <textarea name="body" id="body" class="resize-none border-2 p-3" required rows="10"></textarea>
+                        <div class="flex flex-col mb-4">
+                            <label for="body" class="mb-1 text-sm text-gray-700 uppercase">Body</label>
+                            <textarea name="body" id="body" class="resize-none border-2 p-3" required
+                                rows="10"></textarea>
 
-                                @error('title')
-                                    <div class="text-red-500">{{ $message }}</div>
-                                @enderror
-                            </div>
+                            @error('title')
+                            <div class="text-red-500">{{ $message }}</div>
+                            @enderror
+                        </div>
 
-                            {{-- <div class="flex">
-                                <select id="pet-select">
-                                    @foreach ($ta as $item)
+                        <div class="mb-4">
+                            <label for="tag-select"></label>
 
-                                    @endforeach
-                                    <option value="">--Please choose an option--</option>
-                                    <option value="dog">Dog</option>
-                                    <option value="cat">Cat</option>
-                                    <option value="hamster">Hamster</option>
-                                    <option value="parrot">Parrot</option>
-                                    <option value="spider">Spider</option>
-                                    <option value="goldfish">Goldfish</option>
-                                </select>
-                            </div> --}}
+                            <select name="tags[]" id="tag-select" multiple>
+                                @foreach ($tags as $tag)
+                                    <option value="{{$tag->id}}">{{$tag->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
 
-                            <input type="submit" value="Create Thread" class="py-2 px-4 mr-4 text-white bg-teal-600 rounded cursor-pointer hover:bg-teal-500 focus:bg-teal-500">
-                            <a href="{{ route('forum.index') }}" class="text-teal-500 hover:">Cancel</a>
-                        </form>
-                    </div>
-
+                        <input type="submit" value="Create Thread"
+                            class="py-2 px-4 mr-4 text-white bg-teal-600 rounded cursor-pointer hover:bg-teal-500 focus:bg-teal-500">
+                        <a href="{{ route('forum.index') }}" class="text-teal-500 hover:">Cancel</a>
+                    </form>
                 </div>
+
             </div>
         </div>
     </div>
+</div>
 @endsection
-
