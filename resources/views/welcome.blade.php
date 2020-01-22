a8d23f1ca73e8ee8b74eaa512f2eeaaeef2c3e72
WittyJudge <kros8925@gmail.com>
2020-01-22 10:44:43 +0000

    Add code for welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,16 @@
 
 @section('content')
 <div class="container mx-auto">
-    <h1>{{ $threadCount }}</h1>
+    <div class="flex flex-col items-center">
+        <h1 class="mb-4 text-2xl text-gray-700">You are welcome!</h1>
+        <div class="flex">
+            <a href="{{route('register')}}"
+                class="mr-4 py-4 px-8 text-lg text-white bg-teal-600 hover:bg-teal-500 rounded">Join to
+                us</a>
+            <a href="{{route('forum.index')}}"
+                class="py-4 px-8 text-lg text-white bg-gray-600 hover:bg-gray-500 rounded">Visit the
+                Forum</a>
+        </div>
+    </div>
 </div>
 @endsection
